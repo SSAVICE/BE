f7bfd8029fbcf0ffdfe94c0ec75654705c382b00
hjinshin <gudwls818@gmail.com>
2025-12-09 21:10:25 +0900

    Feat: init

---
+++ src/main/java/teamssavice/ssavice/SsaviceApplication.java
@@ -0,0 +1,13 @@
+package teamssavice.ssavice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsaviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsaviceApplication.class, args);
+	}
+
+}
