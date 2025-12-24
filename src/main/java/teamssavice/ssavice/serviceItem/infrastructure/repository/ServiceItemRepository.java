2056ce78857c72bcafae8a8823065bbc6ed78bbb
BonggusCha <aayy4321@naver.com>
2025-12-24 12:43:42 +0900

    Revert "week2/issue#17 서비스 등록 기능 구현 "

--- src/main/java/teamssavice/ssavice/serviceItem/infrastructure/repository/ServiceItemRepository.java
+++
@@ -1,7 +0,0 @@
-package teamssavice.ssavice.serviceItem.infrastructure.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import teamssavice.ssavice.serviceItem.entity.ServiceItem;
-
-public interface ServiceItemRepository extends JpaRepository<ServiceItem, Long> {
-}
