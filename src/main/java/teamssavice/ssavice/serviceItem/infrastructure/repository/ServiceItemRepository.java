afd38a679d08e5b0e8a3640aa1705a15ed07957a
BonggusCha <aayy4321@naver.com>
2025-12-29 22:06:26 +0900

    Feat: 쿼리 DSL 관련 Repository

--- src/main/java/teamssavice/ssavice/serviceItem/infrastructure/repository/ServiceItemRepository.java
+++ src/main/java/teamssavice/ssavice/serviceItem/infrastructure/repository/ServiceItemRepository.java
@@ -3,5 +3,7 @@ package teamssavice.ssavice.serviceItem.infrastructure.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import teamssavice.ssavice.serviceItem.entity.ServiceItem;
 
-public interface ServiceItemRepository extends JpaRepository<ServiceItem, Long> {
+public interface ServiceItemRepository extends JpaRepository<ServiceItem, Long>, ServiceItemRepositoryCustom {
+
+
 }
