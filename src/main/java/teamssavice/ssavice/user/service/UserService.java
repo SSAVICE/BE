fc666c55b4470427d8d1b14763198863866d632d
momnpa333 <momnpa333@gmail.com>
2026-01-04 20:11:07 +0900

    Feat: service에 modifyProfile 메서드 추가

--- src/main/java/teamssavice/ssavice/user/service/UserService.java
+++ src/main/java/teamssavice/ssavice/user/service/UserService.java
@@ -2,10 +2,12 @@ package teamssavice.ssavice.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import teamssavice.ssavice.auth.Token;
 import teamssavice.ssavice.auth.constants.Role;
 import teamssavice.ssavice.auth.service.TokenService;
 import teamssavice.ssavice.user.entity.Users;
+import teamssavice.ssavice.user.service.dto.UserCommand;
 import teamssavice.ssavice.user.service.dto.UserModel;
 
 @Service
@@ -29,10 +31,25 @@ public class UserService {
         return UserModel.Login.from(token);
     }
 
+    @Transactional(readOnly = true)
     public UserModel.Info getProfile(Long userId) {
         // 사용자 정보 조회
         Users user = userReadService.findByIdFetchJoinAddress(userId);
 
+        return UserModel.Info.from(user);
+    }
+
+    @Transactional
+    public UserModel.Info modifyProfile(Long userId, UserCommand.Modify command) {
+        // 사용자 정보 조회
+        Users user = userReadService.findById(userId);
+
+        // 이메일이 변경되는 경우만 중복 체크
+        if (!user.getEmail().equals(command.email())) {
+            userReadService.checkEmailExists(command.email());
+        }
+
+        user.modify(command.name(), command.email(), command.phoneNumber());
         return UserModel.Info.from(user);
     }
 }
