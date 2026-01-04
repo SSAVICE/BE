7f71f56d8da41d0847d669e1b52ba9e06cd3d71f
momnpa333 <momnpa333@gmail.com>
2026-01-04 18:53:18 +0900

    Feat: UserResponse에 Profile 추가

--- src/main/java/teamssavice/ssavice/user/controller/dto/UserResponse.java
+++ src/main/java/teamssavice/ssavice/user/controller/dto/UserResponse.java
@@ -1,5 +1,6 @@
 package teamssavice.ssavice.user.controller.dto;
 
+import java.sql.Timestamp;
 import lombok.Builder;
 import teamssavice.ssavice.user.service.dto.UserModel;
 
@@ -7,16 +8,43 @@ public class UserResponse {
 
     @Builder
     public record Login(
-            String accessToken,
-            Long expiresIn,
-            String refreshToken
+        String accessToken,
+        Long expiresIn,
+        String refreshToken
     ) {
+
         public static Login from(UserModel.Login model) {
             return Login.builder()
-                    .accessToken(model.accessToken())
-                    .expiresIn(model.expiresIn())
-                    .refreshToken(model.refreshToken())
-                    .build();
+                .accessToken(model.accessToken())
+                .expiresIn(model.expiresIn())
+                .refreshToken(model.refreshToken())
+                .build();
+        }
+    }
+
+    @Builder
+    public record Profile(
+        String imageUrl,
+        String name,
+        Timestamp createdAt,
+        String email,
+        String phoneNumber,
+        String postCode,
+        String address,
+        String detailAddress
+    ) {
+
+        public static Profile from(UserModel.Profile model) {
+            return Profile.builder()
+                .imageUrl(model.imageUrl())
+                .name(model.name())
+                .createdAt(model.createdAt())
+                .email(model.email())
+                .phoneNumber(model.phoneNumber())
+                .postCode(model.postCode())
+                .address(model.address())
+                .detailAddress(model.detailAddress())
+                .build();
         }
     }
 }
