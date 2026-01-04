6fcbfdbb4c3eab5ff13c27f2bedd04d4b22513b0
momnpa333 <momnpa333@gmail.com>
2026-01-04 18:51:30 +0900

    Feat: UserModel에 profile 추가

--- src/main/java/teamssavice/ssavice/user/service/dto/UserModel.java
+++ src/main/java/teamssavice/ssavice/user/service/dto/UserModel.java
@@ -1,22 +1,53 @@
 package teamssavice.ssavice.user.service.dto;
 
+import java.sql.Timestamp;
 import lombok.Builder;
 import teamssavice.ssavice.auth.Token;
+import teamssavice.ssavice.user.entity.Users;
 
 public class UserModel {
 
     @Builder
     public record Login(
-            String accessToken,
-            Long expiresIn,
-            String refreshToken
+        String accessToken,
+        Long expiresIn,
+        String refreshToken
     ) {
+
         public static Login from(Token token) {
             return Login.builder()
-                    .accessToken(token.accessToken())
-                    .expiresIn(token.expiresIn())
-                    .refreshToken(token.refreshToken())
-                    .build();
+                .accessToken(token.accessToken())
+                .expiresIn(token.expiresIn())
+                .refreshToken(token.refreshToken())
+                .build();
+        }
+    }
+
+    @Builder
+    public record Profile(
+        String imageUrl,
+        String name,
+        Timestamp createdAt,
+        String email,
+        String phoneNumber,
+        String postCode,
+        String address,
+        String detailAddress
+    ) {
+
+        public static Profile from(
+            Users user
+        ) {
+            return Profile.builder()
+                .imageUrl(user.getImageUrl())
+                .name(user.getName())
+                .createdAt(Timestamp.valueOf(user.getCreatedAt()))
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .postCode(user.getAddress().getPostCode())
+                .address(user.getAddress().getAddress())
+                .detailAddress(user.getAddress().getDetailAddress())
+                .build();
         }
     }
 }
