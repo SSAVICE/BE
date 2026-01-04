12cf41861168eb466ed43f345d29d60f050d5e79
momnpa333 <momnpa333@gmail.com>
2026-01-05 00:20:45 +0900

    Feat: findAllByUserIdAndStatus 제작

--- src/main/java/teamssavice/ssavice/book/infrastructure/repository/BookRepository.java
+++ src/main/java/teamssavice/ssavice/book/infrastructure/repository/BookRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import teamssavice.ssavice.book.constants.BookStatus;
 import teamssavice.ssavice.book.entity.Book;
 
 
@@ -13,12 +14,26 @@ import teamssavice.ssavice.book.entity.Book;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Query(value = "SELECT b FROM Book b " +
+        "JOIN FETCH b.user " +
+        "JOIN FETCH b.serviceItem s " +
+        "JOIN FETCH s.company " +
+        "JOIN FETCH s.address " +
+        "WHERE b.user.id = :userId",
+
+        countQuery = "SELECT count(b) FROM Book b WHERE b.user.id = :userId")
+    Page<Book> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query(
+        value = "SELECT b FROM Book b " +
             "JOIN FETCH b.user " +
             "JOIN FETCH b.serviceItem s " +
             "JOIN FETCH s.company " +
             "JOIN FETCH s.address " +
-            "WHERE b.user.id = :userId",
-
-            countQuery = "SELECT count(b) FROM Book b WHERE b.user.id = :userId")
-    Page<Book> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
+            "WHERE b.user.id = :userId " +
+            "AND (:status IS NULL OR b.bookStatus = :status)",
+        countQuery = "SELECT count(b) FROM Book b " +
+            "WHERE b.user.id = :userId " +
+            "AND (:status IS NULL OR b.bookStatus = :status)"
+    )
+    Page<Book> findAllByUserIdAndStatus(Long userId, BookStatus status, Pageable pageable);
 }
