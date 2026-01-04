14b2ee6278292c2c5616d428b32dfda74893fb52
momnpa333 <momnpa333@gmail.com>
2026-01-04 18:51:06 +0900

    Feat: controller에 profile 조회 api 추가

--- src/main/java/teamssavice/ssavice/user/controller/UserController.java
+++ src/main/java/teamssavice/ssavice/user/controller/UserController.java
@@ -3,10 +3,12 @@ package teamssavice.ssavice.user.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import teamssavice.ssavice.global.annotation.CurrentId;
 import teamssavice.ssavice.user.controller.dto.UserRequest;
 import teamssavice.ssavice.user.controller.dto.UserResponse;
 import teamssavice.ssavice.user.service.UserService;
@@ -16,14 +18,25 @@ import teamssavice.ssavice.user.service.dto.UserModel;
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
+
     private final UserService userService;
 
     @PostMapping("/login")
     public ResponseEntity<UserResponse.Login> login(
-            @RequestBody @Valid UserRequest.Login request
+        @RequestBody @Valid UserRequest.Login request
     ) {
         UserModel.Login model = userService.register(request.token());
 
         return ResponseEntity.ok(UserResponse.Login.from(model));
     }
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserResponse.Profile> profile(
+        @CurrentId Long userId
+    ) {
+        UserModel.Profile model = userService.getProfile(userId);
+
+        return ResponseEntity.ok(UserResponse.Profile.from(model));
+    }
+
 }
