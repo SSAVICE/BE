59b9bde4d233a5761873be96759242c7d1fb281c
momnpa333 <momnpa333@gmail.com>
2026-01-05 00:20:10 +0900

    Feat: 엔드포인트 중복으로 인한 주석 처리 및 엔드포인트 생성

--- src/main/java/teamssavice/ssavice/book/controller/BookController.java
+++ src/main/java/teamssavice/ssavice/book/controller/BookController.java
@@ -2,15 +2,16 @@ package teamssavice.ssavice.book.controller;
 
 
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import teamssavice.ssavice.auth.constants.Role;
+import teamssavice.ssavice.book.constants.BookStatus;
 import teamssavice.ssavice.book.controller.dto.BookResponse;
 import teamssavice.ssavice.book.service.BookService;
 import teamssavice.ssavice.book.service.dto.BookCommand;
@@ -28,17 +29,34 @@ public class BookController {
 
     private final BookService bookService;
 
+//    @GetMapping("/book")
+//    public ResponseEntity<PageResponse<BookResponse.Info>> getMyBook(
+//        @CurrentId Long userId,
+//        @PageableDefault(size = 10) Pageable pageable
+//    ) {
+//
+//        BookCommand.Retrieve command = BookCommand.Retrieve.of(userId, pageable);
+//
+//        Page<BookModel.Info> models = bookService.getMyBooks(command);
+//        Page<BookResponse.Info> reponsePage = models.map(BookResponse.Info::from);
+//
+//        return ResponseEntity.ok(PageResponse.from(reponsePage));
+//    }
+
     @GetMapping("/book")
-    public ResponseEntity<PageResponse<BookResponse.Info>> getMyBook(
-            @CurrentId Long userId,
-            @PageableDefault(size = 10) Pageable pageable
+    public ResponseEntity<PageResponse<BookResponse.Info>> getMyBooksByStatus(
+        @CurrentId Long userId,
+        @PageableDefault(size = 10) Pageable pageable,
+        @RequestParam(required = true) BookStatus status
     ) {
 
-        BookCommand.Retrieve command = BookCommand.Retrieve.of(userId, pageable);
+        BookCommand.RetrieveByStatus command = BookCommand.RetrieveByStatus.of(userId, pageable,
+            status);
 
-        Page<BookModel.Info> models = bookService.getMyBooks(command);
+        Page<BookModel.Info> models = bookService.getMyBooksByStatue(command);
         Page<BookResponse.Info> reponsePage = models.map(BookResponse.Info::from);
 
         return ResponseEntity.ok(PageResponse.from(reponsePage));
     }
+
 }
