80b9c21f8a123ec228436c00bd2ed6ba4dca328c
momnpa333 <momnpa333@gmail.com>
2026-01-05 00:21:00 +0900

    Feat: getMyBooksByStatue 제작

--- src/main/java/teamssavice/ssavice/book/service/BookService.java
+++ src/main/java/teamssavice/ssavice/book/service/BookService.java
@@ -4,6 +4,7 @@ package teamssavice.ssavice.book.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import teamssavice.ssavice.book.entity.Book;
 import teamssavice.ssavice.book.service.dto.BookCommand;
 import teamssavice.ssavice.book.service.dto.BookModel;
@@ -17,6 +18,17 @@ public class BookService {
 
     public Page<BookModel.Info> getMyBooks(BookCommand.Retrieve command) {
         Page<Book> books = bookReadService.findAllByUserId(command.userId(), command.pageable());
+
+        return books.map(BookModel.Info::from);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<BookModel.Info> getMyBooksByStatue(BookCommand.RetrieveByStatus command) {
+        Page<Book> books = bookReadService.findAllByUserIdAndStatus(
+            command.userId(),
+            command.status(),
+            command.pageable()
+        );
 
         return books.map(BookModel.Info::from);
     }
