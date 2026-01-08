aba9cb7ec91d92bcf85dd4106c487d1a8e8f0719
BonggusCha <aayy4321@naver.com>
2026-01-09 02:50:22 +0900

    Fix: loader 삭제하면서 관련 파일 삭제

--- src/main/java/teamssavice/ssavice/global/repository/RegionRepository.java
+++
@@ -1,7 +0,0 @@
-package teamssavice.ssavice.global.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import teamssavice.ssavice.global.entity.Region;
-
-public interface RegionRepository extends JpaRepository<Region, Long> {
-}
