79d61b139c5164bfb1bbe6576317aa4243224cfa
BonggusCha <aayy4321@naver.com>
2026-01-08 15:13:37 +0900

    Fix: 칼럼 변경에 따른 엔티티 수정

--- src/main/java/teamssavice/ssavice/global/entity/Region.java
+++ src/main/java/teamssavice/ssavice/global/entity/Region.java
@@ -20,20 +20,8 @@ public class Region {
     private String regionCode;
 
     @NotNull
-    @Column(nullable = false, length = 5, columnDefinition = "CHAR(5)")
-    private String gugunCode;
-
-    @NotNull
-    @Column(nullable = false)
-    private String displayName; // 화면에 보여줄 이름
-
-    @NotNull
     @Column(nullable = false)
     private String fullAddress;
-
-    @NotNull
-    @Column(nullable = false)
-    private Integer depth;
 
 
 }
