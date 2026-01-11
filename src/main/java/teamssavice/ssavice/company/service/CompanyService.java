9cca57b9ed36a1ba7148f5972263b6b2ff410146
momnpa333 <momnpa333@gmail.com>
2026-01-12 05:08:03 +0900

    Feat: register에 verifyToken 검증 로직 추가

--- src/main/java/teamssavice/ssavice/company/service/CompanyService.java
+++ src/main/java/teamssavice/ssavice/company/service/CompanyService.java
@@ -1,6 +1,7 @@
 package teamssavice.ssavice.company.service;
 
-import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -8,12 +9,16 @@ import org.springframework.transaction.annotation.Transactional;
 import teamssavice.ssavice.auth.Token;
 import teamssavice.ssavice.auth.constants.Role;
 import teamssavice.ssavice.auth.service.TokenService;
-import teamssavice.ssavice.company.controller.dto.CompanyRequest;
 import teamssavice.ssavice.company.entity.Company;
+import teamssavice.ssavice.company.infrastructure.dto.CompanyInfraCommand;
+import teamssavice.ssavice.company.infrastructure.dto.CompanyInfraModel;
 import teamssavice.ssavice.company.service.client.BusinessVerificationClient;
 import teamssavice.ssavice.company.service.dto.CompanyCommand;
 import teamssavice.ssavice.company.service.dto.CompanyModel;
-
+import teamssavice.ssavice.company.token.CompanySignupVerifyToken;
+import teamssavice.ssavice.company.token.CompanySignupVerifyTokenService;
+import teamssavice.ssavice.global.constants.ErrorCode;
+import teamssavice.ssavice.global.exception.InvalidBusinessNumberException;
 import teamssavice.ssavice.imageresource.constants.ImageConstants;
 import teamssavice.ssavice.imageresource.entity.ImageResource;
 import teamssavice.ssavice.imageresource.service.ImageReadService;
@@ -27,12 +32,10 @@ import teamssavice.ssavice.user.entity.Users;
 import teamssavice.ssavice.user.service.UserReadService;
 import teamssavice.ssavice.user.service.UserWriteService;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class CompanyService {
+
     private final ApplicationEventPublisher applicationEventPublisher;
     private final TokenService tokenService;
     private final UserReadService userReadService;
@@ -44,6 +47,8 @@ public class CompanyService {
     private final ImageReadService imageReadService;
     private final S3Service s3Service;
     private final BusinessVerificationClient businessVerificationClient;
+    private final CompanySignupVerifyTokenService companySignupVerifyTokenService;
+
 
     public CompanyModel.Login login(String kakaoToken) {
         // 토큰 검증
@@ -51,11 +56,11 @@ public class CompanyService {
 
         // user 저장 및 중복 체크
         Users user = userReadService.findByEmail(email)
-                .orElseGet(() -> userWriteService.save(email));
+            .orElseGet(() -> userWriteService.save(email));
 
         // 업체가 있으면 업체 토큰, 없으면 유저 토큰
         Optional<Company> optionalCompany = companyReadService.findByUser(user);
-        if(optionalCompany.isEmpty()) {
+        if (optionalCompany.isEmpty()) {
             Token token = tokenService.issueToken(user.getId(), Role.USER);
             return CompanyModel.Login.from(token, false);
         }
@@ -66,6 +71,9 @@ public class CompanyService {
     }
 
     public CompanyModel.Login register(CompanyCommand.Create command) {
+        companySignupVerifyTokenService.validate(
+            command.userId(), command.businessNumber(), command.verifyToken());
+
         Users user = userReadService.findById(command.userId());
         companyReadService.checkUserExists(user);
 
@@ -83,38 +91,49 @@ public class CompanyService {
     @Transactional(readOnly = true)
     public CompanyModel.MyCompany getMyCompany(Long id) {
         Company company = companyReadService.findByIdFetchJoinAddressAndImageResource(id);
-        List<ServiceItem> services = serviceItemReadService.findTop5ByCompanyOrderByDeadlineDesc(company);
+        List<ServiceItem> services = serviceItemReadService.findTop5ByCompanyOrderByDeadlineDesc(
+            company);
         if (company.hasImageResource()) {
-            String presignedUrl = s3Service.generateGetPresignedUrl(company.getImageResource().getObjectKey());
+            String presignedUrl = s3Service.generateGetPresignedUrl(
+                company.getImageResource().getObjectKey());
             return CompanyModel.MyCompany.from(company, presignedUrl, services);
         }
-        return CompanyModel.MyCompany.from(company, ImageConstants.DEFAULT_COMPANY_IMAGE_OBJECT_KEY, services);
+        return CompanyModel.MyCompany.from(company, ImageConstants.DEFAULT_COMPANY_IMAGE_OBJECT_KEY,
+            services);
     }
 
     @Transactional(readOnly = true)
     public CompanyModel.Info getCompanyById(Long id) {
         Company company = companyReadService.findByIdFetchJoinAddressAndImageResource(id);
-        List<ServiceItem> services = serviceItemReadService.findTop5ByCompanyOrderByDeadlineDesc(company);
-        List<Review> reviews = reviewReadService.findTop3ByCompanyIdOrderByCreatedAt(company.getId());
+        List<ServiceItem> services = serviceItemReadService.findTop5ByCompanyOrderByDeadlineDesc(
+            company);
+        List<Review> reviews = reviewReadService.findTop3ByCompanyIdOrderByCreatedAt(
+            company.getId());
 
         if (company.hasImageResource()) {
-            String presignedUrl = s3Service.generateGetPresignedUrl(company.getImageResource().getObjectKey());
+            String presignedUrl = s3Service.generateGetPresignedUrl(
+                company.getImageResource().getObjectKey());
             return CompanyModel.Info.from(company, presignedUrl, services, reviews);
         }
-        return CompanyModel.Info.from(company, ImageConstants.DEFAULT_COMPANY_IMAGE_OBJECT_KEY, services, reviews);
+        return CompanyModel.Info.from(company, ImageConstants.DEFAULT_COMPANY_IMAGE_OBJECT_KEY,
+            services, reviews);
     }
 
     @Transactional(readOnly = true)
     public CompanyModel.Summary getCompanySummary(Long id) {
         Company company = companyReadService.findByIdFetchJoinAddressAndImageResource(id);
-        List<Review> reviews = reviewReadService.findTop3ByCompanyIdOrderByCreatedAt(company.getId());
+        List<Review> reviews = reviewReadService.findTop3ByCompanyIdOrderByCreatedAt(
+            company.getId());
         Float companyRate = 10F;
         Long rateCount = 100L;
         if (company.hasImageResource()) {
-            String presignedUrl = s3Service.generateGetPresignedUrl(company.getImageResource().getObjectKey());
-            return CompanyModel.Summary.from(company, presignedUrl, companyRate, rateCount, reviews);
+            String presignedUrl = s3Service.generateGetPresignedUrl(
+                company.getImageResource().getObjectKey());
+            return CompanyModel.Summary.from(company, presignedUrl, companyRate, rateCount,
+                reviews);
         }
-        return CompanyModel.Summary.from(company, ImageConstants.DEFAULT_COMPANY_IMAGE_OBJECT_KEY, companyRate, rateCount, reviews);
+        return CompanyModel.Summary.from(company, ImageConstants.DEFAULT_COMPANY_IMAGE_OBJECT_KEY,
+            companyRate, rateCount, reviews);
     }
 
     @Transactional
@@ -122,7 +141,8 @@ public class CompanyService {
         Company company = companyReadService.findByIdFetchJoinImageResource(companyId);
         ImageResource imageResource = imageReadService.findByObjectKey(objectKey);
         if (company.hasImageResource()) {
-            applicationEventPublisher.publishEvent(S3EventDto.UpdateTag.from(company.getImageResource().getObjectKey(), false));
+            applicationEventPublisher.publishEvent(
+                S3EventDto.UpdateTag.from(company.getImageResource().getObjectKey(), false));
         }
         company.updateImage(imageResource);
         applicationEventPublisher.publishEvent(S3EventDto.UpdateTag.from(objectKey, true));
@@ -141,4 +161,5 @@ public class CompanyService {
 
         return CompanyModel.Validate.from(verifyToken);
     }
+
 }
