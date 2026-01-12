fb8a08d70d467a3a3a79379f8085a10f2d252f11
Kwon Da woon <82216606+momnpa333@users.noreply.github.com>
2026-01-12 16:57:28 +0900

    Week5/issue#64 Swagger에 enum 정리, ErrorMassage에 가능한 값 목록 반환, status ALL 추가 (#65)
    * Refactor: BookStatusFilter 적용
    
    도메인의 Enum을 사용하기 보다는 request용 enum활용
    
    * Refactor: 허용 mimeType 목록 반환 메서드 추가 및 ErrorResponse에 반환
    
    * Docs: example에 허용 값 띄우기
    
    * Feat: UnsupportedImageContentTypeException에 가능한 목록 반환 메서드 추가
    
    * Feat: GlobalExceptionHandler에 UnsupportedImageContentTypeException 추가

--- src/main/java/teamssavice/ssavice/global/exception/UnsupportedImageContentTypeException.java
+++ src/main/java/teamssavice/ssavice/global/exception/UnsupportedImageContentTypeException.java
@@ -5,8 +5,24 @@ import teamssavice.ssavice.global.constants.ErrorCode;
 public class UnsupportedImageContentTypeException extends CustomException {
 
     private static final String DEFAULT_TITLE = "Validation Error";
+    private final String invalidValue;
+    private final String[] allowedValues;
 
-    public UnsupportedImageContentTypeException(ErrorCode errorCode) {
+    public UnsupportedImageContentTypeException(
+        ErrorCode errorCode,
+        String invalidValue,
+        String[] allowedValues
+    ) {
         super(errorCode, DEFAULT_TITLE);
+        this.invalidValue = invalidValue;
+        this.allowedValues = allowedValues;
+    }
+
+    public String getInvalidValue() {
+        return invalidValue;
+    }
+
+    public String[] getAllowedValues() {
+        return allowedValues;
     }
 }
