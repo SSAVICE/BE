638f13e0c047ba131bfd74e9bd5121ad96264cad
BonggusCha <aayy4321@naver.com>
2026-01-13 15:16:54 +0900

    Feat: 엔티티 상태 수정

--- src/main/java/teamssavice/ssavice/serviceItem/constants/ServiceStatus.java
+++ src/main/java/teamssavice/ssavice/serviceItem/constants/ServiceStatus.java
@@ -1,12 +1,14 @@
 package teamssavice.ssavice.serviceItem.constants;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum ServiceStatus {
     RECRUITING("모집 중"),
-    FINISHED("종료");
+    SUCCEEDED("모집 성공"),
+    FAILED("모집 실패"),
+    CANCELED("모집 취소"),
+    COMPLETED("이용 완료");
 
     private final String description;
-
-    ServiceStatus(String description) {
-        this.description = description;
-    }
 }
