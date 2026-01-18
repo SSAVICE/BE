0b36fc778f118744785c787285a702793950b2ff
BonggusCha <aayy4321@naver.com>
2026-01-18 17:54:03 +0900

    Feat: 확장성 고려해서 우선 Refund Map만들어서 관리 - 결제 외부 API 연동하면서 수정필요

--- src/main/java/teamssavice/ssavice/refund/service/RefundService.java
+++ src/main/java/teamssavice/ssavice/refund/service/RefundService.java
@@ -1,11 +1,9 @@
 package teamssavice.ssavice.refund.service;
 
 import teamssavice.ssavice.book.entity.Book;
-import teamssavice.ssavice.refund.entity.RefundInfo;
 import teamssavice.ssavice.serviceItem.entity.Price;
 
 import java.util.List;
-import java.util.Map;
 
 public interface RefundService {
     void registerRefunds(List<Book> targetBooks, Price price);
