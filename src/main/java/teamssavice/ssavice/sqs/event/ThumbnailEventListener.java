3d1740cbbfcefcbb23004ddef43d14d7f5765a5a
momnpa333 <momnpa333@gmail.com>
2026-01-26 20:07:43 +0900

    refact: 네이밍 수정 AlarmEvent->eventListner

--- src/main/java/teamssavice/ssavice/sqs/event/ThumbnailEventListener.java
+++ src/main/java/teamssavice/ssavice/sqs/event/ThumbnailEventListener.java
@@ -3,16 +3,16 @@ package teamssavice.ssavice.sqs.event;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import teamssavice.ssavice.sqs.service.ThumbnailAlarmService;
+import teamssavice.ssavice.sqs.service.ThumbnailService;
 
 @Component
 @RequiredArgsConstructor
-public class ThumbnailAlarmEvent {
+public class ThumbnailEventListener {
 
-    private final ThumbnailAlarmService thumbnailAlarmService;
+    private final ThumbnailService thumbnailService;
 
     @EventListener
     public void alarmListener(SqsEventDto.ThumbnailEvent event) {
-        thumbnailAlarmService.handle(event);
+        thumbnailService.metaDataEditAsConfirm(event);
     }
 }
