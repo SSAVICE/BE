a78bf3e800e62c7569aaafd88edf2dd8f5317366
momnpa333 <momnpa333@gmail.com>
2026-02-02 08:17:07 +0900

    feat: 동시성 처리를 위한 processing 상태 추가

--- src/main/java/teamssavice/ssavice/imageresource/constants/ImageStatus.java
+++ src/main/java/teamssavice/ssavice/imageresource/constants/ImageStatus.java
@@ -3,6 +3,7 @@ package teamssavice.ssavice.imageresource.constants;
 public enum ImageStatus {
     PENDING,   // confirm 되었지만 아직 S3 copy 안 됨
     DONE,      // copy 성공 → 사용 가능
-    FAILED     // copy 실패 → 재시도/정리 대상
+    FAILED,    // copy 실패 → 재시도/정리 대상
+    PROCESSING   // 처리 중(선점됨)
 }
 
