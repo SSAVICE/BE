8ff32c5aa07d2b49e0c2088a0d0419338d6e8ffd
momnpa333 <momnpa333@gmail.com>
2026-02-02 05:58:43 +0900

    feat: AsyncConfig imageExecutor 제작

---
+++ src/main/java/teamssavice/ssavice/global/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package teamssavice.ssavice.global.config;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean
+    public Executor imageExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("image-async-");
+        executor.initialize();
+        return executor;
+    }
+}
