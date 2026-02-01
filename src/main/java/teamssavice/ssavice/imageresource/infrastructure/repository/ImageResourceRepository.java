603b48d3e2b9682793625a72064cfb814a6e1c46
momnpa333 <momnpa333@gmail.com>
2026-02-02 08:16:47 +0900

    feat: 동시성 처리를 위한 cas 메서드 제작

--- src/main/java/teamssavice/ssavice/imageresource/infrastructure/repository/ImageResourceRepository.java
+++ src/main/java/teamssavice/ssavice/imageresource/infrastructure/repository/ImageResourceRepository.java
@@ -3,8 +3,10 @@ package teamssavice.ssavice.imageresource.infrastructure.repository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import teamssavice.ssavice.imageresource.constants.ImageStatus;
 import teamssavice.ssavice.imageresource.entity.ImageResource;
 
 public interface ImageResourceRepository extends JpaRepository<ImageResource, Long> {
@@ -15,4 +17,29 @@ public interface ImageResourceRepository extends JpaRepository<ImageResource, Lo
     List<ImageResource> findAllBySourceKeyIn(@Param("sourceKeys") List<String> sourceKeys);
 
     Optional<ImageResource> findBySourceKey(String tempKey);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(
+        "UPDATE ImageResource i " +
+            "SET i.status = :to " +
+            "WHERE i.id = :id " +
+            "AND i.status IN :from"
+    )
+    int updateStatusIfIn(@Param("id") Long id,
+        @Param("to") ImageStatus to,
+        @Param("from") List<ImageStatus> from);
+
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(
+        "UPDATE ImageResource i " +
+            "SET i.status = :to, " +
+            "    i.isActive = :isActive " +
+            "WHERE i.id = :id " +
+            "AND i.status = :onlyWhen"
+    )
+    int updateStatusAndActiveWhen(@Param("id") Long id,
+        @Param("to") ImageStatus to,
+        @Param("isActive") boolean isActive,
+        @Param("onlyWhen") ImageStatus onlyWhen);
 }
