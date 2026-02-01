74459d6207668416368d1928eda92c7d6d250473
momnpa333 <momnpa333@gmail.com>
2026-02-02 06:12:24 +0900

    feat: ImageAsyncService 적용

--- src/main/java/teamssavice/ssavice/s3/event/S3EventHandler.java
+++ src/main/java/teamssavice/ssavice/s3/event/S3EventHandler.java
@@ -4,20 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
+import teamssavice.ssavice.imageresource.service.ImageAsyncService;
 import teamssavice.ssavice.s3.S3Service;
 
 @Component
 @RequiredArgsConstructor
 public class S3EventHandler {
+
     private final S3Service s3Service;
+    private final ImageAsyncService imageAsyncService;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void moveImageEventListener(S3EventDto.Move event) {
-        s3Service.moveObject(event.sourceKey(), event.targetKey(), event.contentType());
+        imageAsyncService.moveAsync(event.sourceKey());
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void deleteImageEventListener(S3EventDto.Delete event) {
-        s3Service.deleteObject(event.objectKey());
+        s3Service.deleteObject(event.targetKey());
     }
 }
