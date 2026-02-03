1a75cefa9d203d795711b790d63fc873416af7f0
hjinshin <gudwls818@gmail.com>
2026-02-04 02:27:33 +0900

    Chore: 불필요한 import 삭제

--- src/main/java/teamssavice/ssavice/serviceItem/infrastructure/repository/ServiceItemRepository.java
+++ src/main/java/teamssavice/ssavice/serviceItem/infrastructure/repository/ServiceItemRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import teamssavice.ssavice.company.entity.Company;
 import teamssavice.ssavice.serviceItem.constants.ServiceStatus;
 import teamssavice.ssavice.serviceItem.entity.ServiceItem;
 
