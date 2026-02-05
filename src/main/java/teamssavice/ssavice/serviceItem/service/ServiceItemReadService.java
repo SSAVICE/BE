d0186559cb4777332787eedf972bd947f12c7544
hjinshin <gudwls818@gmail.com>
2026-02-05 17:34:48 +0900

    Fix: merge 오류 해결

--- src/main/java/teamssavice/ssavice/serviceItem/service/ServiceItemReadService.java
+++ src/main/java/teamssavice/ssavice/serviceItem/service/ServiceItemReadService.java
@@ -41,11 +41,6 @@ public class ServiceItemReadService {
     }
 
     @Transactional(readOnly = true)
-    public ServiceItem getReferenceById(Long serviceId) {
-        return serviceItemRepository.getReferenceById(serviceId);
-    }
-
-    @Transactional(readOnly = true)
     public Page<ServiceItem> findByCompanyAndStatus(ServiceItemCommand.RetrieveByCompanyAndStatus command) {
         return serviceItemRepository.findByCompanyAndStatus(command.companyId(), command.status(), command.pageable());
     }
