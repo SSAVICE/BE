ad4ea5a6ea83284fa93f3dacd073182257945f5b
hjinshin <gudwls818@gmail.com>
2026-02-09 23:49:55 +0900

    Week8/issue#120 리뷰 목록 불러옥 API 구현 (#121)
    * Feat: 자기 회사의 리뷰 목록 불러오기 API 구현
    
    * Feat: 본인의 리뷰 목록 불러오기 API 구현
    
    * Feat: 특정 사용자의 리뷰 목록 불러오기 API 구현
    
    * Refactor: Review에 User와 ServiceItem 연관 관계 맺기
    
    * Fix: 오타 수정

--- src/main/java/teamssavice/ssavice/review/service/dto/ReviewModel.java
+++ src/main/java/teamssavice/ssavice/review/service/dto/ReviewModel.java
@@ -9,17 +9,20 @@ public class ReviewModel {
 
     @Builder
     public record Item(
+            Long userId,
             String userName,
             String comment,
+            String serviceId,
             String serviceName,
             LocalDateTime createdAt,
             Integer rating
     ) {
         public static ReviewModel.Item from(Review review) {
             return Item.builder()
-                    .userName(review.getUserName().masked())
+                    .userId(review.getId())
+                    .userName(review.getUser().getName())
                     .comment(review.getComment())
-                    .serviceName(review.getServiceName())
+                    .serviceName(review.getServiceItem().getTitle())
                     .createdAt(review.getCreatedAt())
                     .rating(review.getRating())
                     .build();
