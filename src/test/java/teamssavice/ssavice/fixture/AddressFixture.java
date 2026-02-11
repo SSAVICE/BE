f5e06aa2bef0dad3576cd4a3fa5b090e383e0d50
momnpa333 <momnpa333@gmail.com>
2026-02-12 03:27:10 +0900

    test: geohash관련 fixture 변경

--- src/test/java/teamssavice/ssavice/fixture/AddressFixture.java
+++ src/test/java/teamssavice/ssavice/fixture/AddressFixture.java
@@ -1,15 +1,19 @@
 package teamssavice.ssavice.fixture;
 
 import teamssavice.ssavice.address.Address;
+import teamssavice.ssavice.global.util.GeoHashUtil;
 
 import java.math.BigDecimal;
 
 public class AddressFixture {
 
     public static Address address() {
+        BigDecimal lat = BigDecimal.valueOf(33.333);
+        BigDecimal lon = BigDecimal.valueOf(33.333);
         return Address.builder()
-                .latitude(BigDecimal.valueOf(33.333))
-                .longitude(BigDecimal.valueOf(33.333))
+                .latitude(lat)
+                .longitude(lon)
+                .geoHash(GeoHashUtil.encode(lat, lon))
                 .postCode("01234")
                 .address("address")
                 .detailAddress("detailAdd")
