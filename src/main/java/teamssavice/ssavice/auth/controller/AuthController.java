b5368aef4b19e60b19ce2ed897b568580d54350e
momnpa333 <momnpa333@gmail.com>
2026-03-02 00:27:32 +0900

    feat: token으로 사용자 id 반환 api

--- src/main/java/teamssavice/ssavice/auth/controller/AuthController.java
+++ src/main/java/teamssavice/ssavice/auth/controller/AuthController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 import teamssavice.ssavice.auth.controller.dto.AuthResponse;
 import teamssavice.ssavice.auth.service.TokenService;
 import teamssavice.ssavice.auth.service.dto.AuthModel;
+import teamssavice.ssavice.global.annotation.CurrentAuth;
 import teamssavice.ssavice.global.annotation.CurrentRefreshToken;
 import teamssavice.ssavice.global.annotation.PermitAll;
+import teamssavice.ssavice.global.dto.Auth;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,7 +22,7 @@ public class AuthController {
     @PermitAll
     @GetMapping("token/refresh")
     public ResponseEntity<AuthResponse.Refresh> refresh(
-            @CurrentRefreshToken String refreshToken
+        @CurrentRefreshToken String refreshToken
     ) {
         AuthModel.Refresh model = tokenService.refresh(refreshToken);
         return ResponseEntity.ok(AuthResponse.Refresh.from(model));
@@ -29,9 +31,16 @@ public class AuthController {
     @PermitAll
     @GetMapping("/logout")
     public ResponseEntity<Void> logout(
-            @CurrentRefreshToken String refreshToken
+        @CurrentRefreshToken String refreshToken
     ) {
         tokenService.logout(refreshToken);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<AuthResponse.Me> me(
+        @CurrentAuth Auth auth
+    ) {
+        return ResponseEntity.ok(AuthResponse.Me.from(auth));
     }
 }
